299e3cd9da86755bf3427b7dbfdad8541a2ea10e
Gabriel Rodrigues <gabriel.rodriguesxs@gmail.com>
2023-12-31 10:52:52 -0300

    :sparkles: feat: Adiciona a base do projeto Spring

---
+++ src/test/java/com/generation/hortifruti/HortifrutiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.hortifruti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HortifrutiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
